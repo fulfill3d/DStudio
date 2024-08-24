13f3c87656834051acead3ecf55bf46842385ef5
Abdurrahman Gazi Yavuz <sniper.erratic.0w@icloud.com>
2024-08-23 23:58:14 -0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import {Provider} from "react-redux";
+import store from "@/store/store";
+import {DesignStudio} from "@/containers/DesignStudio";
+import {validateToken} from "@/hooks/common/useValidateToken";
+import {useEffect, useState} from "react";
+
+export default function Home() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [message, setMessage] = useState("Unauthorized");
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const token = urlParams.get('token');
+
+        if (token) {
+            validateToken(token).then(isValid => {
+                setIsAuthenticated(isValid);
+            }).catch(error => {
+                setIsAuthenticated(false);
+                setMessage(error.message);
+            });
+        }
+    }, []);
+
+    if (!isAuthenticated) {
+        return <div>{message}</div>;
+    }
+
+  return (
+      <Provider store={store}>
+        <DesignStudio/>
+      </Provider>
+  );
+}
