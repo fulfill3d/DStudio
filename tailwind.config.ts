2f1a1c0ba1d589c3e6deff37888f4f3f812d811f
Abdurrahman Gazi Yavuz <sniper.erratic.0w@icloud.com>
2024-10-21 22:06:40 -0700

    tailwind css config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: 'jit',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,67 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        violet: {
+          100: "#A5B4FB",
+          200: "#A8A6FF",
+          300: "#918efa",
+          400: "#807dfa",
+        },
+        pink: {
+          200: "#FFA6F6",
+          300: "#fa8cef",
+          400: "#fa7fee",
+        },
+        red: {
+          200: "#FF9F9F",
+          300: "#fa7a7a",
+          400: "#f76363",
+        },
+        orange: {
+          200: "#FFC29F",
+          300: "#FF965B",
+          400: "#fa8543",
+        },
+        yellow: {
+          200: "#FFF59F",
+          300: "#FFF066",
+          400: "#FFE500",
+        },
+        lime: {
+          100: "#c6fab4",
+          200: "#B8FF9F",
+          300: "#9dfc7c",
+          400: "#7df752",
+        },
+        cyan: {
+          200: "#A6FAFF",
+          300: "#79F7FF",
+          400: "#53f2fc",
+        },
       },
+      width: {
+        '1/10': '10%',
+        '2/10': '20%',
+        '3/10': '30%',
+        '4/10': '40%',
+        '5/10': '50%',
+        '6/10': '60%',
+        '7/10': '70%',
+        '8/10': '80%',
+        '9/10': '90%',
+      },
+      height: {
+        '1/10': '10%',
+        '2/10': '20%',
+        '3/10': '30%',
+        '4/10': '40%',
+        '5/10': '50%',
+        '6/10': '60%',
+        '7/10': '70%',
+        '8/10': '80%',
+        '9/10': '90%',
+      }
     },
   },
   plugins: [],
